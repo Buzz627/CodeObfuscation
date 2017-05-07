534f07c1281897e32d4be13ca19c26e26b44d7ff
Buzz <buzz627@gmail.com>
2017-05-07 12:23:05 -0400

    made test script

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # CodeObfuscation
+
+## Brain storming
+ 12:00 pm
+The first think I am going to do is create a test file in js so i can actually ru this thing
+then read in the file and look for non reserve words that are not in quotes. so all the variables and function names. 
+create a function that will create random strings then replace all variables and function names with the corrisponding random word
+
+I think ill do this in python just to make it a little easier for reading the file and whatnot.
+
---
+++ testScript.js
@@ -0,0 +1,34 @@
+
+//this is a test function
+function thisIsAFunction(inputVar){
+	console.log("this is a test")
+	console.log(inputVar)
+	//add 3
+	inputVar+=3
+	console.log(inputVar)
+
+}
+
+function testBool(anotherInput){
+	if (anotherInput<4){
+		return true
+	}
+	//else is not necissory here just want to add more reserve words
+	else{
+		return false
+	}
+}
+
+//call a function
+thisIsAFunction(3)
+//simple for loop
+for (var forLoopVar=0; forLoopVar<5; forLoopVar++){
+	console.log(forLoopVar)
+}
+var hereIsAThing =0
+while (testBool(hereIsAThing)){
+	console.log("all is good")
+	hereIsAThing+=1
+}
+var finished="Done!"
+console.log(finished)
