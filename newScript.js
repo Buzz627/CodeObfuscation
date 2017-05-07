65311d19371ec7dd3e39bd929fcd615c0ceedf19
Buzz <buzz627@gmail.com>
2017-05-07 14:32:19 -0400

    formatting

--- newScript.js
+++ newScript.js
@@ -1,7 +1,7 @@
-function z4un(ko9xe0qll){console.log('this is a test');
-console.log(ko9xe0qll);ko9xe0qll+=3;console.log(ko9xe0qll);
-}function z3lywmyea(c7si){if (c7si<4){return true ;
-}else{return false;}}z4un(3);for (var a9rag9a=0; a9rag9a<5; a9rag9a++){
-console.log(a9rag9a);}var wny2ky3 =0;while (z3lywmyea(wny2ky3)){
-console.log("all is good");wny2ky3+=1;}var zt3fa0="Done!";
-console.log(zt3fa0);
+function w2thm1hrdy(a5mws1pu){console.log('this is a test');
+console.log(a5mws1pu);a5mws1pu+=3;console.log(a5mws1pu);
+}function aqi8rdhbjtj(yafjlpxs){if (yafjlpxs<4){return true ;
+}else{return false;}}w2thm1hrdy(3);for (var axx8jx4wsd=0; axx8jx4wsd<5; axx8jx4wsd++){
+console.log(axx8jx4wsd);}var pzpyi =0;while (aqi8rdhbjtj(pzpyi)){
+console.log("all is good");pzpyi+=1;}var fpwfs2aahif="Done!";
+console.log(fpwfs2aahif);
